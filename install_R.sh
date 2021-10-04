624e6b4e18c12b772aaaaf9ec8349338f416409f
ianaraujo <ianaraujo15@gmail.com>
2021-10-04 15:47:53 -0300

    Install R and RStudio

---
+++ install_R.sh
@@ -0,0 +1,22 @@
+sudo apt-get update 
+
+# Install dependencies
+
+sudo apt install dirmngr gnupg gnupg-agent apt-transport-https ca-certificates software-properties-common -y
+
+# Install R
+
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/'
+
+sudo apt install r-base -y
+sudo apt install r-base-dev -y
+sudo apt install build-essential -y
+
+# Install RStudio
+
+sudo apt-get install gdebi
+
+cd Downloads/
+wget https://download1.rstudio.org/desktop/bionic/amd64/rstudio-2021.09.0-351-amd64.deb
+sudo gdebi rstudio-2021.09.0-351-amd64.deb -n
