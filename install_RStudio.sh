2d6020245f44f1be074c75dfff8a9155bf03d234
ianaraujo <ianaraujo15@gmail.com>
2021-10-18 08:16:56 -0300

    Create RStudio install script

---
+++ install_RStudio.sh
@@ -0,0 +1,7 @@
+# Install RStudio
+
+sudo apt-get install gdebi -y
+
+cd Downloads/
+wget https://download1.rstudio.org/desktop/bionic/amd64/rstudio-2021.09.0-351-amd64.deb
+sudo gdebi rstudio-2021.09.0-351-amd64.deb -n
