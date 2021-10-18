5406489d9346c5284baa6973e26f60229f336261
ianaraujo <ianaraujo15@gmail.com>
2021-10-18 08:17:36 -0300

    Remove lines with RStudio install code

--- install_R.sh
+++ install_R.sh
@@ -9,13 +9,6 @@ sudo apt install dirmngr gnupg gnupg-agent apt-transport-https ca-certificates s
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
 sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/'
 
-sudo apt install r-base-dev -y
 sudo apt install build-essential -y
+sudo apt install r-base-dev -y
 
-# Install RStudio
-
-sudo apt-get install gdebi -y
-
-cd Downloads/
-wget https://download1.rstudio.org/desktop/bionic/amd64/rstudio-2021.09.0-351-amd64.deb
-sudo gdebi rstudio-2021.09.0-351-amd64.deb -n
