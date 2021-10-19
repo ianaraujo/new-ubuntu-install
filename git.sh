7af0467a000be9a0ceb391febb82257b71ed5d0f
ianaraujo <ianaraujo15@gmail.com>
2021-10-19 13:37:20 -0300

    Create git config bash script

---
+++ git.sh
@@ -0,0 +1,17 @@
+# Setting username
+
+git config --global user.name ianaraujo
+
+echo "Setting git username 'ianaraujo': OK"
+
+# Setting email
+
+git config --global user.email ianaraujo15@gmail.com
+
+echo "Setting git email 'ianaraujo15@gmail.com': OK"
+
+# ...
+
+git config --global color.ui true
+
+
