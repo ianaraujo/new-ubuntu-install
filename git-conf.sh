bc1611423d0899bbd752faea4eda639a40ae5e5a
ianaraujo <ianaraujo15@gmail.com>
2021-10-19 13:30:23 -0300

    Add git global config file

---
+++ git-conf.sh
@@ -0,0 +1,13 @@
+# Setting username
+
+git config --global user.name ianaraujo
+
+echo "Setting git username 'ianaraujo': OK"
+
+# Setting email
+
+git config --global user.email ianaraujo15@gmail.com
+
+echo "Setting git email 'ianaraujo15@gmail.com': OK"
+
+
