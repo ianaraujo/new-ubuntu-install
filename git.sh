5e1b3a1d277268c8d0db35b578a4b4d2079f1dac
Ian Vaz Araujo <ianaraujo15@gmail.com>
2022-02-01 18:51:09 -0300

    Generate pub ssh key

--- git.sh
+++ git.sh
@@ -10,8 +10,12 @@ git config --global user.email ianaraujo15@gmail.com
 
 echo "Setting git email 'ianaraujo15@gmail.com': OK"
 
-# ...
+# color.ui
 
 git config --global color.ui true
 
+# ssh keygen
 
+ssh-keygen -t ed25519 -C 'ianaraujo15@gmail.com'
+
+echo "SSH Public Key:" cat .ssh/id_ed25519.pub
